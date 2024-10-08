c24119c05ac2333eb83bbc3cfa9bd7692bc9e156
HolaDav <oladejidavid12@gmail.com>
2024-10-08 19:10:01 +0100

    Mapping URLs with Params

--- Django Web Framework/WORKPLACE/place/myproject/myapp/urls.py
+++ Django Web Framework/WORKPLACE/place/myproject/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home),
+    path('dishes/<str:dish>', views.menuitems)
 ]
--- Django Web Framework/WORKPLACE/place/myproject/myapp/views.py
+++ Django Web Framework/WORKPLACE/place/myproject/myapp/views.py
@@ -26,3 +26,14 @@ def home(request):
 
     return HttpResponse(msg, content_type='text/html', charset='utf-8')
     # return response
+
+def menuitems(request, dish):
+    items = {
+        'pasta': 'Pasta is a type of noodle made from combination of wheat, water or eggs.',
+        'falafel': 'Falafel are deep fried patties or balls made from',
+        'cheesecake': 'Cheesecake is a type of dessert made with cream, soft cheese on top of cookie, pastry crust or graham cracker and fruit sauce topping.'
+    }
+
+    description = items[dish]
+
+    return HttpResponse(f"<h2> {dish} </h2>" + description)
