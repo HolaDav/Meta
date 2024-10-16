a1fec270c27b82fda7d8cc15308d80cfd2d170b5
HolaDav <oladejidavid12@gmail.com>
2024-10-16 17:20:50 +0100

    Working with migrations

--- Django Web Framework/WORKPLACE/place/myproject/myapp/models.py
+++ Django Web Framework/WORKPLACE/place/myproject/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Menuitems(models.Model):
+    itemname = models.CharField(max_length=200)
+    category = models.CharField(max_length=300)
+    year = models.IntegerField()
